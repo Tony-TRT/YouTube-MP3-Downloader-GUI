da3c06e484162858718f8c2b7720fd146e799a8c
Tony-TRT <146631446+Tony-TRT@users.noreply.github.com>
2024-05-18 16:59:34 +0200

    Add layouts to the window.

--- app.py
+++ app.py
@@ -15,6 +15,26 @@ class MainWindow(AestheticWindow):
         self.setWindowTitle("YouTube MP3 Downloader")
         self.setFixedSize(900, 500)
 
+        ##################################################
+        # Layouts.
+        ##################################################
+
+        self.main_layout = None
+        self.left_layout = None
+        self.right_layout = None
+
+        self.ui_manage_layouts()
+
+    def ui_manage_layouts(self) -> None:
+        """Layouts are managed here."""
+
+        self.main_layout = QtWidgets.QHBoxLayout(self)
+        self.left_layout = QtWidgets.QVBoxLayout()
+        self.right_layout = QtWidgets.QGridLayout()
+
+        self.main_layout.addLayout(self.left_layout)
+        self.main_layout.addLayout(self.right_layout)
+
 
 if __name__ == '__main__':
     root = QtWidgets.QApplication()
